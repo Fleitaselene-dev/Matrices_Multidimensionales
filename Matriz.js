963cebd0360c789254bf32c90fa6582e26c4789f
fleita selene <fleitaselene26@gmail.com>
2025-03-25 15:45:55 -0300

    Se modifico para el ingreso de muchas personas a la vez

--- Matriz.js
+++ Matriz.js
@@ -1,18 +1,23 @@
 //Matriz Multidimencional
 
 console.log("Ingrese sus siguientes datos")
-let nombre = prompt("Ingrese su nombre:")
-let apellido = prompt("Ingrese su apellido:")
-let dni = prompt("Ingrese su dni:")
+let matriz = []
 
 //Numero telefonico 
-let numero = []
+
 while(true){
     console.log("Para salir ingrese: Terminado")
-    let numeroTelefonico = prompt("Ingrese su numero telefonico")
-    if(numeroTelefonico == "Terminado"){break}
-    numero.push(numeroTelefonico)
+    let nombre = prompt("Ingrese su nombre:")
+    if(nombre == "Terminado") {break};
+    let apellido = prompt("Ingrese su apellido:")
+    let dni = prompt("Ingrese su dni:")
+    //Lista de numero telefonico
+    let numero = []
+    while(true){
+      let numeroTelefonico = prompt("Ingrese su numero telefonico")
+      if(numeroTelefonico == "Terminado"){break}
+    numero.push(numeroTelefonico)}
+    matriz.push([nombre, apellido, dni, numero])
 }
-let matriz = [nombre, apellido, dni, numero]
-//Datos en consola
+
 console.log(matriz)
--- Matriz.py
+++ Matriz.py
@@ -1,18 +1,27 @@
 #Matriz multidimencional
 
-nombre = input("Ingrese su nombre:")
-apellido = input("Ingrese su apellido:")
-dni = input("Ingrese su dni:")
 
-#Cargas de numero telefonico
+matriz = []
 
-numero = []
 while True:
-    print("Para salir escriba:'Terminado'")
-    numero_telefono = input("Ingrese su numeor de telefono:")
-    if numero_telefono == "Terminado":
+   print("Ingrese sus datos(Para salir escriba: 'Terminado'")
+   nombre = input("Ingrese su nombre:")
+   if nombre == "Terminado":
+      print("A salido")
+      break
+   apellido = input("Ingrese su apellido:")
+   dni = input("Ingrese su dni:")
+   #Cargas de numero telefonico
+   numero = []
+   while True:
+     print("Para salir escriba:'Terminado'")
+     numero_telefono = input("Ingrese su numeor de telefono:")
+     if numero_telefono == "Terminado":
         print("A salido")
         break
-    numero.append(numero_telefono)
-matriz = [nombre , apellido , dni , numero]
-print(matriz)
+     #Se agrega numero de telefono a la lista numero
+     numero.append(numero_telefono)
+   matriz.append([nombre, apellido, dni, numero])
+print("\nMatriz final:")
+for persona in matriz:
+    print(persona)
